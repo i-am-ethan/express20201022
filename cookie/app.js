0fd60285e7f4812a01ecdf4d33d018228a068f64
ethan <izawaryosuke0101@gmail.com>
2020-10-22 16:59:11 +0900

    cookie

---
+++ cookie/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var ejs = require('ejs');
+var app = express();
+//cookieparseオブジェクトの生成
+var cookieParser = require('cookie-parser');
+app.engine('ejs', ejs.renderFile);
+//cookie-parserの利用宣言
+app.use(cookieParser());
+app.get('/',function(req,res){
+  var cnt = req.cookies.cnt == undefined ? 0:req.cookies.cnt;
+  cnt++;
+  res.cookie('cnt', cnt, {
+    maxAge:5000
+  });
+  res.render('temp.ejs', {
+    cnt:cnt
+  });
+});
+
+var server = app.listen(1234, function(){
+  console.log('サーバを起動しました');
+})
