5a25d32ac67d1b3398f67c3afb62ce8374fed5b0
ethan <izawaryosuke0101@gmail.com>
2020-10-22 15:30:07 +0900

    sendData

---
+++ sendData/app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var ejs = require('ejs');
+//bodyParserを使うためのオブジェクト生成
+var bodyParser = require('body-parser');
+var app = express();
+
+app.engine('ejs', ejs.renderFile);
+//bodyParserの初期化
+app.use(bodyParser.urlencoded({
+  extended:true
+}));
+
+app.get('/', function(req,res){
+  console.log('---GET Request---');
+  console.log('nameは'+req.query.name);
+  console.log('ageは'+req.query.age);
+  res.render('temp.ejs', {});
+});
+
+app.post('/', function(req,res){
+  console.log('---POST Request---');
+  console.log('nameは'+req.body.name);
+  console.log('ageは'+req.body.age);
+  res.render('temp.ejs', {});
+});
+
+var server = app.listen(1234, function(){
+  console.log('サーバを起動しました');
+});
+
+
